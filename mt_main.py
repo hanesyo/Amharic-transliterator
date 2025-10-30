4732229f826408e901cee27b62e031e4b5101242
hanesyo <132240513+hanesyo@users.noreply.github.com>
2025-10-30 17:16:45 -0400

    Delete mt_main.py

--- mt_main.py
+++
@@ -1,7 +0,0 @@
-from mt_transliterator import transliterate
-
-sample = "አድርገህልኛልና በቸርነትህ " \
-"አመሰግንሃለሁ እልል"
-sampled = "እግዚአብሔር ከእኛ ጋር ነው።"
-print("Original:", sample)
-print("Phonetic:", transliterate(sample))
